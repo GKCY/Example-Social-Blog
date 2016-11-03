c7ebdd312b6ab58a349facace15a6edea7b81d0f
GKCY <frendchen1997@gmail.com>
2016-11-03 15:51:40 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from config import config
 from flask_login import LoginManager
+from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
@@ -14,6 +14,7 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
     app = Flask(__name__)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db, login_manager
 from flask_login import UserMixin
+from . import db, login_manager
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -11,7 +12,6 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-		
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -27,16 +27,15 @@ class User(UserMixin, db.Model):
 
     @password.setter
     def password(self, password):
-    	self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
-    	return check_password_hash(self.password_hash, password)
-    
-	def __repr__(self):
-		return '<User %r>' % self.username
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @login_manager.user_loader
-def load_use(user_id):
-	return User.query.get(int(user_id))
-     
+def load_user(user_id):
+    return User.query.get(int(user_id))
